ce80991ab32755ce57b4b5aa62ccead8416c763f
neptunehub <g.colangiuli@gmail.com>
2025-07-14 20:37:15 +0200

    try to show config page

--- Jellyfin.Plugin.AudioMuseAi/Plugin.cs
+++ Jellyfin.Plugin.AudioMuseAi/Plugin.cs
@@ -9,15 +9,15 @@ using MediaBrowser.Model.Serialization;
 namespace Jellyfin.Plugin.AudioMuseAi
 {
     /// <summary>
-    /// The main plugin class.
+    /// Main entry point for the AudioMuse AI plugin.
     /// </summary>
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="Plugin"/> class.
         /// </summary>
-        /// <param name="applicationPaths">The application paths.</param>
-        /// <param name="xmlSerializer">The XML serializer.</param>
+        /// <param name="applicationPaths">Application paths for embedded resources.</param>
+        /// <param name="xmlSerializer">XML serializer for configuration.</param>
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
             : base(applicationPaths, xmlSerializer)
         {
@@ -25,9 +25,9 @@ namespace Jellyfin.Plugin.AudioMuseAi
         }
 
         /// <summary>
-        /// Gets the plugin instance.
+        /// Gets the singleton instance of the plugin.
         /// </summary>
-        public static Plugin? Instance { get; private set; }
+        public static Plugin Instance { get; private set; }
 
         /// <inheritdoc />
         public override string Name => "AudioMuse AI";
@@ -36,15 +36,16 @@ namespace Jellyfin.Plugin.AudioMuseAi
         public override Guid Id => Guid.Parse("e3831be1-c025-4ebc-bc79-121ad0dfc4e1");
 
         /// <inheritdoc />
+        public override string Description => "Provides integration with the AudioMuse AI backend service.";
+
+        /// <inheritdoc />
         public IEnumerable<PluginPageInfo> GetPages()
         {
-            return new[]
+            yield return new PluginPageInfo
             {
-                new PluginPageInfo
-                {
-                    Name = Name,
-                    EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.configPage.html"
-                }
+                Name = "AudioMuse AI",
+                // Path to the embedded HTML configuration page
+                EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
             };
         }
     }

1621cb582406908598a591db7ce8246ba1832f8c
neptunehub <g.colangiuli@gmail.com>
2025-07-15 12:04:42 +0200

    config page fix

--- Jellyfin.Plugin.AudioMuseAi/Plugin.cs
+++ Jellyfin.Plugin.AudioMuseAi/Plugin.cs
@@ -5,47 +5,38 @@ using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
+using MediaBrowser.Controller.Chapters;
+using MediaBrowser.Controller.Configuration;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Entities;
 
 namespace Jellyfin.Plugin.AudioMuseAi
 {
-    /// <summary>
-    /// Main entry point for the AudioMuse AI plugin.
-    /// </summary>
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Plugin"/> class.
-        /// </summary>
-        /// <param name="applicationPaths">Application paths for embedded resources.</param>
-        /// <param name="xmlSerializer">XML serializer for configuration.</param>
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
             : base(applicationPaths, xmlSerializer)
         {
             Instance = this;
         }
 
-        /// <summary>
-        /// Gets the singleton instance of the plugin.
-        /// </summary>
         public static Plugin Instance { get; private set; }
 
-        /// <inheritdoc />
         public override string Name => "AudioMuse AI";
-
-        /// <inheritdoc />
         public override Guid Id => Guid.Parse("e3831be1-c025-4ebc-bc79-121ad0dfc4e1");
+        public override string Description => "Integrates Jellyfin with an AudioMuse AI backend.";
 
-        /// <inheritdoc />
-        public override string Description => "Provides integration with the AudioMuse AI backend service.";
-
-        /// <inheritdoc />
         public IEnumerable<PluginPageInfo> GetPages()
         {
+            // must exactly match the EmbeddedResource name in the DLL:
+            // Jellyfin.Plugin.AudioMuseAi.Configuration.configPage.html
             yield return new PluginPageInfo
             {
                 Name = "AudioMuse AI",
-                // Path to the embedded HTML configuration page
-                EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
+                EmbeddedResourcePath = 
+                    $"{GetType().Namespace}.Configuration.configPage.html"
+
             };
         }
     }

315da3166e9d6e7e1378dfa0d25b21451fa502e5
neptunehub <g.colangiuli@gmail.com>
2025-07-18 15:26:31 +0200

    BUILD

--- Jellyfin.Plugin.AudioMuseAi/Plugin.cs
+++ Jellyfin.Plugin.AudioMuseAi/Plugin.cs
@@ -37,6 +37,9 @@ namespace Jellyfin.Plugin.AudioMuseAi
         public override string Description => "Integrates Jellyfin with an AudioMuse AI backend.";
 
         /// <inheritdoc />
+        public override string ImageUrl => $"embedded:{GetType().Namespace}.audiomuseai.png";
+
+        /// <inheritdoc />
         public IEnumerable<PluginPageInfo> GetPages()
         {
             yield return new PluginPageInfo
